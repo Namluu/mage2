17dce9114d0150c16c994ae1289fa40d876fe600
Nam Luu <nam.luuduc@gmail.com>
2015-12-31 16:22:45 +0700

    use log class

--- app/code/Tomluu/Logpathinfo/Model/Observer.php
+++ app/code/Tomluu/Logpathinfo/Model/Observer.php
@@ -5,9 +5,17 @@ use Magento\Framework\Event\ObserverInterface;
 
 class Observer implements ObserverInterface
 {
+    protected $logger;
+
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $info = $observer->getRequest()->getPathInfo();
+        //$this->logger->addDebug($info);
         //var_dump($info);
         //die('aaaa');
     }
