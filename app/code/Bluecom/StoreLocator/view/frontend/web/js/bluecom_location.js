39d90448c0b85de67e80106af1b070e9dfb7556e
Nam Luu <nam.luuduc@gmail.com>
2016-02-01 15:51:06 +0700

    update location find direction

--- app/code/Bluecom/StoreLocator/view/frontend/web/js/bluecom_location.js
+++ app/code/Bluecom/StoreLocator/view/frontend/web/js/bluecom_location.js
@@ -1,7 +1,12 @@
 function initAutocomplete()
 {
+    var listLocation = [];
     // init map
-    var defaultLoc = new google.maps.LatLng(10.8034919, 106.6161219);
+    var firstLoc = document.getElementById('ul-list').children[0];
+    var defaultLoc = new google.maps.LatLng(
+        firstLoc.getAttribute('data-longitude'), 
+        firstLoc.getAttribute('data-latitude')
+        );
 
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
@@ -12,7 +17,7 @@ function initAutocomplete()
     var map = new google.maps.Map(mapCanvas, mapOptions);
 
     // show location marker on map
-    showLocations(map);
+    listLocation = showLocations(map);
 
     // Create the search box and link it to the UI element.
     var input = document.getElementById('addressInput');
@@ -54,13 +59,20 @@ function initAutocomplete()
 
             map.setCenter(place.geometry.location);
 
+            find_closest_marker(listLocation, place.geometry.location, map);
+
         });
+
+
+
     });
     // [END region_getplaces]
 }
 
 function showLocations(map)
 {
+    listLocation = [];
+
     var locs = document.getElementById('ul-list').children;
     Array.prototype.forEach.call(locs, function(el, i){
         var position = new google.maps.LatLng(
@@ -92,6 +104,55 @@ function showLocations(map)
 
             map.setCenter(position);
         });
+
+        listLocation.push(position);
     });
+
+    return listLocation;
+
 }
 
+function rad(x) {return x*Math.PI/180;}
+function find_closest_marker( list, current, map ) {
+    var lat = current.lat();
+    var lng = current.lng();
+    var R = 6371; // radius of earth in km
+    var distances = [];
+    var closest = -1;
+    for( i=0;i<list.length; i++ ) {
+        var mlat = list[i].lat();
+        var mlng = list[i].lng();
+        var dLat  = rad(mlat - lat);
+        var dLong = rad(mlng - lng);
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        distances[i] = d;
+        if ( closest == -1 || d < distances[closest] ) {
+            closest = i;
+        }
+    }
+
+
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+        map: map
+    });
+
+    // Set destination, origin and travel mode.
+    var request = {
+        destination: list[closest],
+        origin: current,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    // Pass the directions request to the directions service.
+    var directionsService = new google.maps.DirectionsService();
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            // Display the route on the map.
+            directionsDisplay.setDirections(response);
+        }
+    });
+
+}
